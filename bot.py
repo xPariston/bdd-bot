d031ade0b827ea9fe15227e2456f36dae66b8ea0
Patrick <Patrickwiechert94@yahoo.de>
2019-01-12 12:54:06 +0100

    c

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ async def StateWars(context):
     Totalwarurllist = []
 
     for stateid in stateids:
-        Totalwarurllist += await rrDamage.getStateWars7d(stateids,days)
+        Totalwarurllist.extend(await rrDamage.getStateWars7d(stateids,days))
     # for id in stateids:
     #     warlist= await rrDamage.getStateWars(id,days)
     #     for war in warlist:
