75da56bde328380563cfb20185beaa606d727bc7
Patrick <Patrickwiechert94@yahoo.de>
2019-01-22 12:26:55 +0100

    s

--- rrDamage.py
+++ rrDamage.py
@@ -13,7 +13,7 @@ myheader = \
         "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
         "Cache-Control": "max-age=0",
         "Connection": "keep-alive",
-        "Cookie": "__cfduid=d69d7469bb2cfead36e2b8f70fef951071545946806; _iub_cs-76236742=%7B%22consent%22%3Atrue%2C%22timestamp%22%3A%222018-12-27T22%3A07%3A36.972Z%22%2C%22version%22%3A%221.2.4%22%2C%22id%22%3A76236742%7D; fbm_1457231197822920=base_domain=.rivalregions.com; PHPSESSID=gpkqvovoqlu1olqhf0pho22ue2; __atuvc=4%7C52%2C2%7C1%2C4%7C2%2C3%7C3; __atuvs=5c43a2721c96b144000; rr=0616af8500a633aa85acba0df947124e; rr_id=2000268192; rr_add=151117b3509ac7758c52a27dd00ca409",
+        "Cookie": "__cfduid=dbbfd4861308b729c8488cf2d6fec21481548156348; PHPSESSID=ikid6idiqptn0f4lird7c6dvi1; __atuvc=1%7C4; __atuvs=5c46fdbbbbfb445e000; _iub_cs-76236742=%7B%22consent%22%3Atrue%2C%22timestamp%22%3A%222019-01-22T11%3A26%3A04.486Z%22%2C%22version%22%3A%221.2.4%22%2C%22id%22%3A76236742%2C%22documentClicked%22%3Atrue%7D; rr=865c634fa27382332f70fcd181ae69d7; rr_id=2000268192; rr_add=151117b3509ac7758c52a27dd00ca409",
         "Host": "rivalregions.com",
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
